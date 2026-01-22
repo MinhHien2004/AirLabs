aa7b464ce0c8ff57fb1d3f9c3a14b3e5684e6c64
hienminh1332004 <nguyenthanhjkluio@gmail.com>
2026-01-22 15:44:10 +0700

    KKK

--- src/Scheduled.tsx
+++ src/Scheduled.tsx
@@ -169,7 +169,7 @@ const Scheduled: React.FC = () => {
         <input
           type="text"
           className="iata"
-          placeholder="Press the airport iata Test 1"
+          placeholder="Press the airport iata KKKK"
           value={iata}
           onChange={handleIataChange}
         />

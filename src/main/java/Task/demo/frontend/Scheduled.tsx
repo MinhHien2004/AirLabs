e2b548e2a370c69ea8d5db0870d25dacf5cf5e6c
hienminh1332004 <nguyenthanhjkluio@gmail.com>
2026-01-23 15:04:52 +0700

    Check webhook 3

--- src/main/java/Task/demo/frontend/Scheduled.tsx
+++ src/main/java/Task/demo/frontend/Scheduled.tsx
@@ -169,7 +169,7 @@ const Scheduled: React.FC = () => {
         <input
           type="text"
           className="iata"
-          placeholder="Fill Airport IATA second"
+          placeholder="Fill Airport IATA third"
           value={iata}
           onChange={handleIataChange}
         />

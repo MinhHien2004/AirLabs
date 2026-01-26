e6cde7d395607169c26b8136628ac52ebc9b632b
hienminh1332004 <nguyenthanhjkluio@gmail.com>
2026-01-26 22:03:08 +0700

    Test merge 1

--- src/main/java/Task/demo/frontend/Scheduled.tsx
+++ src/main/java/Task/demo/frontend/Scheduled.tsx
@@ -169,7 +169,7 @@ const Scheduled: React.FC = () => {
         <input
           type="text"
           className="iata"
-          placeholder="Fill Airport IATA third fff"
+          placeholder="Test merge"
           value={iata}
           onChange={handleIataChange}
         />

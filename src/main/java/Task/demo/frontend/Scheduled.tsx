42a0c4b5c364d3a3f7528a3ebe0272e7075ebb8c
hienminh1332004 <nguyenthanhjkluio@gmail.com>
2026-01-26 16:59:34 +0700

    Tes deplot

--- src/main/java/Task/demo/frontend/Scheduled.tsx
+++ src/main/java/Task/demo/frontend/Scheduled.tsx
@@ -169,7 +169,7 @@ const Scheduled: React.FC = () => {
         <input
           type="text"
           className="iata"
-          placeholder="Fill Airport IATA third"
+          placeholder="Fill Airport IATA third fff"
           value={iata}
           onChange={handleIataChange}
         />
